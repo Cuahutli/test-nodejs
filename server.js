71702d6824d1084c1c06f3f130161f90d3b4c0da
Cuahutli Miguel Ulloa Robles <Cuahutli Miguel Ulloa Robles>
2015-12-07 12:56:52 -0700

    se hiciero las modificaciones para pasar los test de JSLint en brackets

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 // server.js
-
 // BASE
 // =============================================================================
-
 // Usamos los paquetes que necesitamos
-var express    = require('express');        // Llamamos a Express
-var app        = express();                 // Definimos nuestra App usando Express
-var bodyParser = require('body-parser');
 
+/*jslint node: true */
+/*jshint strict: true */
+
+"use strict";
+var express    = require('express');        // Llamamos a Express
+var bodyParser = require('body-parser');
+var app = express();                 // Definimos nuestra App usando Express
 // Configuramos la app para que use bodyParser()
 // Esto nos permitirá manejar peticiones POST
 app.use(bodyParser());
@@ -19,8 +21,8 @@ var port = process.env.PORT || 3000;        // Indicamos el puerto que vamos a u
 var router = express.Router();              // creamos una instancia del Router de Express
 
 // Creamos la ruta principal.
-router.get('/', function(req, res) {
-    res.send('Hola mundo!');
+router.get('/', function (req, res) {
+	res.send('Hola mundo! con brackets');
 });
 
 // Indicamos a Express que trabajaremos con / como base (Podría ser que todas las rutas quisieramos que iniciaran con /api, por ejemplo)
